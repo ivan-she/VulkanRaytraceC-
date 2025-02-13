382c7019e80c4910f40b9fb6b87008f52e6e2339
Ivan Shevtsov <ivan.shevtsov321@gmail.com>
2025-02-13 14:25:08 +0200

    Shaders

---
+++ compile.bat
@@ -0,0 +1,3 @@
+G:\ApiApp\VulkanSDK\Bin\glslc.exe shaders\simp_shader.vert -o shaders\simp_shader.vert.spv
+G:\ApiApp\VulkanSDK\Bin\glslc.exe shaders\simp_shader.frag -o shaders\simp_shader.frag.spv
+pause
