096b57b69cfc40e4add705e53e84a51105c59dd0
Ivan Shevtsov <ivan.shevtsov321@gmail.com>
2025-02-19 13:26:33 +0200

    Device Setup

--- rt_window.cpp
+++ rt_window.cpp
@@ -1,5 +1,7 @@
 #include "rt_window.hpp"
 
+#include <stdexcept>
+
 namespace rt {
 	RtWindow::RtWindow(int w, int h, std::string name) : width{ w }, height{ h }, windowName(name) 
 	{ initWindow(); }
@@ -16,4 +18,12 @@ namespace rt {
 
 		window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
 	}
+	void RtWindow::createWindowSurface(VkInstance instance, VkSurfaceKHR* surface)
+	{
+		if (glfwCreateWindowSurface(instance, window, nullptr, surface) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create a window surface");
+		}
+	}
+
 }
